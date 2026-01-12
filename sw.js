413b931bb1d42db688e663283da3a9e2cc7f6a30
Jonas19380 <jonasmendessantos6@gmail.com>
2026-01-12 17:17:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "calculadora-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./Calculadora.css",
+  "./Calculadora.js",
+  "./manifest.json",
+  "./click.mp3"
+];
+
+// Instala o service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativa e limpa cache antigo
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
